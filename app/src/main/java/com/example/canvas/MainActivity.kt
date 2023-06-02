a894fec371e45dc9095f210f8ff0367f6cb3c0d0
armaanjaj <armaan.jaj@gmail.com>
2023-06-01 23:28:09 -0600

    feature: ask for user's permission to access the phone's gallery

--- app/src/main/java/com/example/canvas/MainActivity.kt
+++ app/src/main/java/com/example/canvas/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.canvas
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.Dialog
-import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 
@@ -15,6 +19,23 @@ class MainActivity : AppCompatActivity() {
 
     private var drawingView: DrawingView? = null
     private var mImageButtonCurrentPaint: ImageButton? = null
+
+    val requestPermission: ActivityResultLauncher<Array<String>> = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
+        permissions ->
+            permissions.entries.forEach{
+                val permissionName = it.key
+                val isGranted = it.value
+
+                if(isGranted){
+                    Toast.makeText(this@MainActivity, "Permission granted", Toast.LENGTH_SHORT).show()
+                } else {
+                    // always make sure that the Manifest here is imported from 'android', neither from java nor com.examples
+                    if(permissionName == Manifest.permission.READ_EXTERNAL_STORAGE){
+                        Toast.makeText(this@MainActivity, "Oops, you just denied the permission", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +54,11 @@ class MainActivity : AppCompatActivity() {
         val ibBrush: ImageButton = findViewById(R.id.ib_brush)
         ibBrush.setOnClickListener {
             showBrushSizeSelectorDialog()
+        }
+
+        val ibGallery: ImageButton = findViewById(R.id.ib_gallery)
+        ibGallery.setOnClickListener {
+            requestStoragePermission()
         }
     }
 
@@ -73,5 +99,24 @@ class MainActivity : AppCompatActivity() {
 
             mImageButtonCurrentPaint = view
         }
+    }
+
+    private fun requestStoragePermission(){
+        if( ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE) ){
+            showRationaleDialog("Canvas", "Canvas needs to access your external storage")
+        } else {
+            requestPermission.launch( arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE
+                // TODO- Add writing to external storage permission
+            ))
+        }
+    }
+
+    private fun showRationaleDialog(title: String, message: String){
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("Cancel") { dialog, _ -> dialog.dismiss() }
+        builder.create().show()
     }
 }
